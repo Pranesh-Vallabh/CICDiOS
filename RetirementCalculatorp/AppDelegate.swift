5bb1642d063bdada85c899efd38f80bb2665f176
Pranesh Vallabh <pvallabh@oldmutual.com>
2022-10-09 21:21:11 +0200

    Add events

--- RetirementCalculatorp/AppDelegate.swift
+++ RetirementCalculatorp/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
           Analytics.self,
           Crashes.self
         ])
-        
+                
         return true
     }
 
--- RetirementCalculatorp/ViewController.swift
+++ RetirementCalculatorp/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import AppCenterCrashes
+import AppCenterAnalytics
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var monthlyInvestmentsTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var retirementAgeTextField: UITextField!
+    @IBOutlet weak var interestRateTextField: UITextField!
+    @IBOutlet weak var savingsTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if       Crashes.hasCrashedInLastSession {
+            let alert = UIAlertController(title: "Oops", message: "Sorry abot that an error occurred", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "It;s cool", style: .default))
+            self.present(alert, animated: true)
+        }
 
+    }
 
+    @IBAction func calculateButton_TouchUpInside(_ sender: Any) {
+        //Crashes.generateTestCrash()
+        
+        Analytics.trackEvent("calculate_retirement_amount")
+    }
+    
 }
 
